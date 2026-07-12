9df304a41b77d3385ea905cc85520e2921aba5c3
jdragonhx <jayden.dhx@gmail.com>
2026-07-12 04:17:40 +0000

    Jul 12, 2026, 4:17 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,16 +1,1 @@
-function setup() {
-  // Set up the canvas
-  new Canvas(800, 400);
-  background(250);
-  fill("skyblue");
-  stroke("pink");
-  strokeWeight(10);
-
-  circle(30,30,50);
-  rect(50,50,100,200);
-}
-
-function draw() {
-  //type here
-}
-
+let ball;
