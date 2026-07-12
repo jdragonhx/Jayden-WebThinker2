8fc8dfa3ce5e82afca6609167b5ebb982dea8261
jdragonhx <jayden.dhx@gmail.com>
2026-07-12 04:19:57 +0000

    Jul 12, 2026, 4:19 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,1 +1,20 @@
 let ball;
+let box;
+
+function setup() {
+  new Canvas(800, 400);
+  background(250);
+
+  ball = new Sprite();
+  ball.x = 100;
+  ball.y = 200;
+  ball.diameter = 40;
+  ball.color = "blue";
+  
+  box = new Sprite();
+  box.x = 100;
+  box.y = 100;
+  box.width = 50;
+  box.height = 50;
+  box.color = "green";
+}
