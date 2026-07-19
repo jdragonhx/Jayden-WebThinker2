716622548e503069de2e507aed483ff7d41dbe4f
jdragonhx <jayden.dhx@gmail.com>
2026-07-19 03:53:35 +0000

    Jul 19, 2026, 3:53 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -34,7 +34,7 @@ function setup() {
   console.log("Sum of first 10 even numbers is: " + sum);
   text("Sum of first 10 even numbers is: " + sum, 300, 150);
 
-  
+  //print the equasion of the sum of 
 
   // --- Exercise: Age category classification ---
   // write your codes here
