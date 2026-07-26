98386cdccb254febb7dfd6342c6622214b1f706f
jdragonhx <jayden.dhx@gmail.com>
2026-07-26 04:31:02 +0000

    Jul 26, 2026, 4:31 AM

---
+++ flappybird/playground/pg01_setup.js
@@ -0,0 +1,8 @@
+let bird, floor;
+let flapMidImg, bg, base;
+
+function preload() {
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
