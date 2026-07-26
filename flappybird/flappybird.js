8ffd8768d03dc15e843d50ec952c0b4753354b93
jdragonhx <jayden.dhx@gmail.com>
2026-07-26 04:17:46 +0000

    Jul 26, 2026, 4:17 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -4,3 +4,12 @@ function Setup() {
     createCanvas(400, 600);
     background('white');
     }
+
+function Draw() {
+    background('white');
+    fill('black');
+    textSize(20);
+    text("Flappy Bird", 150, 50);
+    textSize(12);
+    text("Click to start", 160, 80);
+    }
