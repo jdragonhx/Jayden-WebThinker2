5db9f9b19f1e0b08c61f4140cf7bfd64d2c1566f
jdragonhx <jayden.dhx@gmail.com>
2026-07-26 04:29:52 +0000

    Jul 26, 2026, 4:29 AM

--- flappybird/flappybird_01.js
+++ flappybird/flappybird_01.js
@@ -6,7 +6,7 @@ let flapMidImg, bg, base;
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
-    base = loadImage('base.png');
+    base = loadImage('assets/base.png');
 }
 
 function setup() {
