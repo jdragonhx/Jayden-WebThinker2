5dc20c3ace4fb9cf5f1a1494c8b5a1941c51562f
jdragonhx <jayden.dhx@gmail.com>
2026-07-26 04:08:53 +0000

    Jul 26, 2026, 4:08 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -7,3 +7,29 @@ function setup() {
     pipeInterval = 100;
     frameCount = 0;
 }
+
+function draw() {
+    background(0, 191, 255);
+    bird.update();
+    bird.show();
+    
+    if (frameCount % pipeInterval === 0) {
+        pipes.push(new Pipe());
+    }
+
+    for (let i = pipes.length - 1; i >= 0; i--) {
+        pipes[i].update();
+        pipes[i].show();
+        
+        if (pipes[i].hits(bird)) {
+            console.log("Game Over");
+            noLoop();
+        }
+
+        if (pipes[i].offscreen()) {
+            pipes.splice(i, 1);
+        }
+    }
+    
+    frameCount++;
+}
