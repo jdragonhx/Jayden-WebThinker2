77eb235c8afeb22455b670cc1f53b1708c37c02a
jdragonhx <jayden.dhx@gmail.com>
2026-07-26 04:09:55 +0000

    Jul 26, 2026, 4:09 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -34,3 +34,61 @@ function draw() {
     frameCount++;
 }
 
+function keyPressed() {
+    if (key === ' ') {
+        bird.up();
+    }
+}   
+
+function Bird() {
+    this.y = height / 2;
+    this.x = 64;
+    this.gravity = 0.6;
+    this.lift = -15;
+    this.velocity = 0;
+    
+    this.show = function() {
+        fill(255);
+        ellipse(this.x, this.y, 32, 32);
+    }
+
+    this.up = function() {
+        this.velocity += this.lift;
+    }
+    
+    this.update = function() {
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+    }
+
+    this.hits = function(pipe) {
+        if (this.y < pipe.top || this.y > height - pipe.bottom) {
+            if (this.x > pipe.x && this.x < pipe.x + pipe.w) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function Pipe() {
+    this.top = random(height / 2);
+    this.bottom = random(height / 2);
+    this.x = width;
+    this.w = 20;
+    this.speed = 2;
+    
+    this.show = function() {
+        fill(34, 139, 34);
+        rect(this.x, 0, this.w, this.top);
+        rect(this.x, height - this.bottom, this.w, this.bottom);
+    }
+    
+    this.update = function() {
+        this.x -= this.speed;
+    }
+    
+    this.offscreen = function() {
+        return (this.x < -this.w);
+    }
+}
