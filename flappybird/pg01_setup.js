656e37e375ac86bc96b7154695ece41def0549d4
jdragonhx <jayden.dhx@gmail.com>
2026-08-02 04:10:06 +0000

    Aug 2, 2026, 4:10 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -38,7 +38,7 @@ function draw() {
     image(bg, 0, 0, width, height);
 
     if (kb.presses('space')) {
-        bird.vel.y = -15;
+        bird.vel.y = -2;
         bird.sleeping = false;
     }
 }
