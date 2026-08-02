6213d355571d02f5ad7f37e2fe61e1487b0dedf0
jdragonhx <jayden.dhx@gmail.com>
2026-08-02 03:22:18 +0000

    Aug 2, 2026, 3:22 AM

--- flappybird/playground/pg01_setup.js
+++ flappybird/playground/pg01_setup.js
@@ -10,7 +10,7 @@ function preload() {
 function setup() {
     new Sprite();
     bird.x = width / 2;
-    bird.y = 200,
+    bird.y = 200;
     bird.width = 30;
     bird.height = 30;
     bird.img = flapMidImg;
