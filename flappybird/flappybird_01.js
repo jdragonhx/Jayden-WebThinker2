a11468cf1f66a8ca85d8b22ea03f49d497db1a86
jdragonhx <jayden.dhx@gmail.com>
2026-08-02 04:23:04 +0000

    Aug 2, 2026, 4:23 AM

--- flappybird/flappybird_01.js
+++ flappybird/flappybird_01.js
@@ -1,11 +1,51 @@
-// Flappy Bird
+let bird, floor;
+let flapMidImg, bg, base;
 
+function preload() {
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
 
 function setup() {
-    createCanvas(400, 600);
-    background('white');
-    }
+    new Canvas(400, 600);
+
+    bird = new Sprite()
+    bird.x = width / 2;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.img = flapMidImg;
+
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.5;
+    world.gravity.y = 10;
+
+
+    floor = new Sprite()
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static"
+    floor.img = base;
+
+}
 
 function draw() {
-    
+    image(bg, 0, 0, width, height);
+
+    if (kb.presses('space')) {
+        bird.vel.y = -10000;
+        bird.sleeping = false;
     }
+
+    fill("blue");
+    textSize(14)
+    text('vel.y:' + bird.vel.y.toFixed(2), 10, 20)
+    text('isMoving: ' + bird.isMoving, 10, 40);
+    text('sleeping:' + bird.sleeping , 10, 60);
+
+}
