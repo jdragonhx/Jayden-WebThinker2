f9ef3a127bd165ab000f7d8431a5f99f92bdcb88
jdragonhx <jayden.dhx@gmail.com>
2026-08-16 03:49:05 +0000

    Aug 16, 2026, 3:49 AM

--- flappybird/flappybird_02.js
+++ flappybird/flappybird_02.js
@@ -3,8 +3,8 @@ let flapMidImg, bg, base;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
-    flapUpImg = loadImage('assets/yellowbird-upflap.png')
-    flapDownImg = loadImage('assets/yellowbird-downflap.png')
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+    flapDownImg = loadImage('assets/yellowbird-downflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
 }
