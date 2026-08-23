734f7563188b2c9a325d4aa899919d0bc7d07d85
jdragonhx <jayden.dhx@gmail.com>
2026-08-23 04:17:37 +0000

    Aug 23, 2026, 4:17 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -14,6 +14,7 @@ function preload() {
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
     pipe = loadImage('assets/pipe-green.png');
+    gameoverImg = loadImage('')
 }
 
 function setup() {
