9d5faa022bb6ca1dabca1afe1b5b1df26c0e2b66
jdragonhx <jayden.dhx@gmail.com>
2026-08-23 04:34:58 +0000

    Aug 23, 2026, 4:34 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -77,12 +77,12 @@ function draw() {
         if (bird.vel.y < -1) {
             bird.img = flapUpImg;
             bird.rotation = -30;
-    }
+        }
     
         else if (bird.vel.y > 1) {
             bird.img = flapDownImg;
             bird.rotation = 30;
-    }
+        }
     
         else {
             bird.img = flapMidImg;
