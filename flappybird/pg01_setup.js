c42655debbced28344553b18300cd80d49d24d8f
jdragonhx <jayden.dhx@gmail.com>
2026-08-23 04:35:31 +0000

    Aug 23, 2026, 4:35 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -108,12 +108,12 @@ function draw() {
             }
 
         if (bird.collides(pipeGroup) || bird.collides(floor)){
-        gameoverLabel = new Sprite (width/2, height/2, 192, 42);
-        gameoverLabel.img = gameoverImg;
-        gameoverLabel.layer = 100;
-        gameoverLabel.x = camera.x;
+            gameoverLabel = new Sprite (width/2, height/2, 192, 42);
+            gameoverLabel.img = gameoverImg;
+            gameoverLabel.layer = 100;
+            gameoverLabel.x = camera.x;
 
-        noLoop();
+            noLoop();
     }
 }
 
