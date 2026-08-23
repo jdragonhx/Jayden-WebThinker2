e7381671f605a5e469284220a535175fbf7846bc
jdragonhx <jayden.dhx@gmail.com>
2026-08-23 03:55:48 +0000

    Aug 23, 2026, 3:55 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -101,7 +101,8 @@ function spawnPipePair(){
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0;
 
-    topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');    topPipe.img = pipe;
+    topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
+    topPipe.img = pipe;
     topPipe.rotation = 180;
 
     pipeGroup.add(topPipe);
