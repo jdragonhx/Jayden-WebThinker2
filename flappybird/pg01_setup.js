cdf7039f5f5c8b8c0e0d00d36305cddd8c75c633
jdragonhx <jayden.dhx@gmail.com>
2026-08-30 04:01:54 +0000

    Aug 30, 2026, 4:01 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -161,6 +161,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
     let startX = x - totalWidth / 2;
 
     for (let i = 0; i < scoreStr.length; i++) {
-        let digit = int(scoreStr[i])
+        let digit = int(scoreStr[i]);
+        let xPos
     }
 }
