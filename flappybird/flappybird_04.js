26ce5d9280e940d068f49472da6f05c3f1511427
jdragonhx <jayden.dhx@gmail.com>
2026-08-30 03:34:36 +0000

    Aug 30, 2026, 3:34 AM

--- flappybird/flappybird_04.js
+++ flappybird/flappybird_04.js
@@ -9,7 +9,8 @@ let gameoverLabel;
 let startScreenLabel;
 let startScreenImg;
 let startGame = false;
-let score 
+let score = 0;
+let numberImages 
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
