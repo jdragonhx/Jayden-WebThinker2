22900b2ef5738e03327c9c78b8ca452a9e49b0a3
jdragonhx <jayden.dhx@gmail.com>
2026-08-30 03:11:05 +0000

    Aug 30, 2026, 3:11 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -55,7 +55,7 @@ function setup() {
 function draw() {
     image(bg, 0, 0, width, height);
 
-       if (kb.presses('space') || MouseEvent.presses()){
+       if (kb.presses('space') || mouse.presses()){
         startGame = true;
         startScreenLabel.visible = false;
     }
