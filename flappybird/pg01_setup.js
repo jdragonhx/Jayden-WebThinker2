3e4fdf1c7881558d598daa312db7162bfbbb7cbd
jdragonhx <jayden.dhx@gmail.com>
2026-08-30 04:21:23 +0000

    Aug 30, 2026, 4:21 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -171,5 +171,8 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
 
 function moveGroup(group, targetX, spacing) {
     let totalWidth = (group.length -1) * spacing;
-    let startX = (targetX - totalWidrh)
+    let startX = (targetX - totalWidth/2);
+    for (let i = 0; i < group.length; i++) {
+        group
+    }
 }
