ffe94aebd6a95003490481f8cc2872f22a89be05
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 04:16:50 +0000

    Sep 13, 2026, 4:16 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -13,7 +13,7 @@ let score = 0;
 let numberImages = [];
 let scoreDigits;
 let wingSound;
-let dieAudio
+let dieAudio;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
@@ -25,7 +25,7 @@ function preload() {
     gameoverImg = loadImage('assets/gameover.png');
     startScreenImg = loadImage('assets/message.png');
     wingSound = createAudio('sfx_wing.mp3');
-    dieAudio 
+    dieAudio = 
 
 
     for (let i = 0; i < 10; i++) {
