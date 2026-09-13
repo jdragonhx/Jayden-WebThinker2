f7bac4c30a6b9777152cc831a63182bf9d4a408d
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 04:15:11 +0000

    Sep 13, 2026, 4:15 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -23,6 +23,7 @@ function preload() {
     pipe = loadImage('assets/pipe-green.png');
     gameoverImg = loadImage('assets/gameover.png');
     startScreenImg = loadImage('assets/message.png');
+    wingSound - lo
 
     for (let i = 0; i < 10; i++) {
         numberImages[i] = loadImage('assets/' + i + '.png')
