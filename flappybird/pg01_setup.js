904450c7de200ad339175c4a1e09b2a45031ed77
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 04:14:05 +0000

    Sep 13, 2026, 4:14 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -12,7 +12,7 @@ let startGame = false;
 let score = 0;
 let numberImages = [];
 let scoreDigits;
-let wing;
+let wing
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
