83df384f69ca8f415d4a830f58208b1967fdcdd6
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 03:46:38 +0000

    Sep 13, 2026, 3:46 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -119,6 +119,8 @@ function draw() {
                 }
             }
 
+            
+
         if (bird.collides(pipeGroup) || bird.collides(floor)){
             gameoverLabel = new Sprite (width/2, height/2, 192, 42);
             gameoverLabel.img = gameoverImg;
