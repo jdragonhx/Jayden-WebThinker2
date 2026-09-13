f0d88ba0a5e3507468c89eaeff2ac8fe9835a0be
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 04:32:24 +0000

    Sep 13, 2026, 4:32 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -159,6 +159,8 @@ function draw() {
                 bird.rotation = 0;
                 bird.collider = 'static';
                 bird.y = 200;
+
+                gameOverLabel
             })
 
 
