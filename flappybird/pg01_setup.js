eb0021c1b6ba363fc3d509e353db6166a7ef70ea
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 04:22:10 +0000

    Sep 13, 2026, 4:22 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -76,7 +76,7 @@ function draw() {
     if (kb.presses('space') || mouse.presses()) {
         startGame = true;
         startScreenLabel.visible = false;
-        
+        wingSound.play
     }
 
     if (startGame) {
