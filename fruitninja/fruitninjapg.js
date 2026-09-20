e2c2e5f9ec5975e8169807ced1ac0bfddb527112
jdragonhx <jayden.dhx@gmail.com>
2026-09-20 04:15:19 +0000

    Sep 20, 2026, 4:15 AM

--- fruitninja/fruitninjapg.js
+++ fruitninja/fruitninjapg.js
@@ -1,2 +1,9 @@
 function preload() {
     dojoBG = loadImage("assets/dojo_background.png");
+}
+
+function setup() {
+    createCanvas(800, 600);
+    fruitninja = new FruitNinja();
+    fruitninja.setup();
+}
